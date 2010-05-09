923a97510f67c196bd29c58bde0c11ec4913e587
Ben XO <me@ben-xo.com>
2010-05-09 14:34:30 +0100

    Add lots of SSLRealtimeModel doc.

--- SSL/SSLRealtimeModel.php
+++ SSL/SSLRealtimeModel.php
@@ -1,9 +1,52 @@
 <?php
 
+/**
+ * The real time model responds to timer ticks via tick(), and changes to the tracks
+ * on the decks via notify(). (Calling notify() does not imply a tick()).
+ * 
+ * This model is intended to simulate the decks in ScratchLIVE, but it does so by inferring
+ * the status of the decks from information abotu TRACKS that are written to the history file.
+ * Therefore it will not always match everything that happens in software exactly. However, 
+ * it can infer quite a lot!
+ * 
+ * We model the decks to be in one of five states:
+ * 
+ * * EMPTY
+ * 		No track has yet been loaded on to the deck. This is the starting state for the deck.
+ * 		(This state is never returned to by any other transition).
+ * 
+ * * NEW		
+ * 		A track was added to the deck, and may (or may not) be playing. At this point it's 
+ * 		unknown	if it will count as a play or not for the purposes of the history (ScratchLive
+ *      itself enters it into the history as 'unplayed' by default). However, we model it as 
+ *      "Now Playing" for the purposes of scrobble-info after waiting NOW_PLAYING_POINT seconds,
+ *      so as to keep it visually interesting...
+ * 
+ * * PLAYING 
+ * 		The track on the deck is still playing, but will definitely count as "played" 
+ * 		in the history. In ScratchLive, tracks transition from NEW to PLAYING when a new track 
+ * 		is loaded on to any OTHER decks. At this point, ScratchLive marks the track as played.
+ * 		We then trigger timers for the various scrobble rules, based on MIN_SCROBBLE_TIME and
+ * 		SCROBBLE_POINT_DIVIDER.
+ * 
+ * * PLAYED	
+ * 		Similar to EMPTY, but indicates that the previous track on the deck has finished playing.
+ * 		This state is usually only reached if the track runs out, or the deck was unloaded while 
+ * 		the state was PLAYING. If the track was simply replaced by a different track, you won't 
+ * 		see this state.
+ * 
+ * * SKIPPED
+ * 		Similar to EMPTY, but the previous track on the deck was not played for the purposes of 
+ * 		the history. This state	is usually only reached if the deck was unloaded while the state 
+ * 		was NEW (implying that you are just trying a track out in the mix, rather than playing it). 
+ * 		If the track was simply replaced by a different track, you won't see this state.
+ * 
+ * @author ben
+ */
 class SSLRealtimeModel
 {
-    const MIN_SCROBBLE_TIME = 10;
-    const NOW_PLAYING_POINT = 15;
+    const MIN_SCROBBLE_TIME = 5;
+    const NOW_PLAYING_POINT = 10;
     const SCROBBLE_POINT_DIVIDER = 16;
     
     /* @var $left_track  SSLTrack */
@@ -29,8 +72,11 @@ class SSLRealtimeModel
         {
             if($this->next_timer[$deck] && $this->next_timer[$deck] <= time())
             {
+                // timer for this track has elapsed!
                 $this->next_timer[$deck] = 0;
                 
+                // switch on the status when the timer goes off
+                // so we don't notify on scrobbler, now played etc multiple times.
                 switch($this->status[$deck])
                 {
                     case 'PLAYED':
@@ -98,6 +144,9 @@ class SSLRealtimeModel
     
     public function notify(SSLHistoryDiffDom $diff)
     {
+        // all timer resets and changes go here
+        $stop = array(); // timers to stop
+        $start = array(); // timers to start
         foreach($diff->getTracks() as $track)
         {
             /* @var $track SSLTrack */
@@ -110,29 +159,20 @@ class SSLRealtimeModel
                 switch($status)
                 {
                     case 'SKIPPED':
-                        $this->end[$deck] = $track->getEndTime();
-                        break;
-                        
-                    case 'NEW':
-                        $this->track[$deck] = $track;
-                        $this->start[$deck] = time();
-                        if(!$this->next_timer[$deck])
-                        {
-                            $this->next_timer[$deck] = time() + self::NOW_PLAYING_POINT;
-                        }
+                        $this->stopDeck($deck, $track);
                         break;
                         
                     case 'PLAYED':
                         $this->previous[$deck] = $track;
-                        $this->end[$deck] = $track->getEndTime();
+                        $this->stopDeck($deck, $track);
                         break;
                         
+                    case 'NEW':
+                        $this->startDeck($deck, $track);
+                        $start[$deck] = time() + self::NOW_PLAYING_POINT;
+                        break;
+                                                
                     case 'PLAYING':
-                        $this->track[$deck] = $track;
-                        if(!$this->next_timer[$deck])
-                        {
-                            $this->next_timer[$deck] = time() + self::NOW_PLAYING_POINT;
-                        }
                         break;
                         
                     default:
@@ -142,8 +182,30 @@ class SSLRealtimeModel
                     $this->status[$deck] = $status;
                 }
             }
-        }        
+        }
+        
+        foreach($stop as $deck => $stop)
+        {
+            $this->next_timer[$deck] = 1;            
+        }
+        
+        foreach($start as $deck => $start)
+        {
+            $this->next_timer[$deck] = $start;
+        }
     }
+    
+    protected function stopDeck($track, $deck)
+    {
+        $this->end[$deck] = $track->getEndTime();
+    }
+    
+    protected function startDeck($track, $deck)
+    {
+        $this->track[$deck] = $track;
+        $this->start[$deck] = time();
+    }
+    
     
     protected function getPrevTrackTitle($deck)
     { 
