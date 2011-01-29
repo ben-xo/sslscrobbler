9e1593f70803684e1c31ac60cfdb58d6a84cca66
Ben XO <me@ben-xo.com>
2011-01-29 23:18:48 +0000

    added Analyser tools which I use with the example stats plugin

---
+++ SSL/Plugins/Analyzer/README.txt
@@ -0,0 +1,3 @@
+Everytime you re-run the analysis you should remove analyse.db and reinitialise it like this:
+
+sqlite3 analyze.db <analyze.sql
---
+++ SSL/Plugins/Analyzer/analyze.sql
@@ -0,0 +1,15 @@
+CREATE TABLE history (
+	row INTEGER PRIMARY KEY,
+	filename VARCHAR,
+	title VARCHAR,
+	artist VARCHAR,
+	deck INTEGER,
+	starttime INTEGER,
+	endtime INTEGER,
+	played INTEGER,
+	updatedAt INTEGER,
+	playtime INTEGER,
+	length VARCHAR,
+	album VARCHAR,
+	fullpath TEXT
+);
