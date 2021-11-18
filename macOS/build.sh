0d093ce99deebf37a3118d9a6586b43e8e01ec54
Ben XO <75862+ben-xo@users.noreply.github.com>
2021-11-18 21:51:51 +0000

    Make platypus build work on MacOS Monterey etc with whatever PHP is installed (#27)

--- macOS/build.sh
+++ macOS/build.sh
@@ -3,7 +3,11 @@
 set -e
 
 if [[ `which platypus` == "" ]]; then
-	echo "Please install platypus through Mac Homebrew"
+    echo "Please install platypus through Mac Homebrew"
+fi
+
+if [[ `which php` == "" ]]; then
+    echo "Please install php through Mac Homebrew"
 fi
 
 VERSION=$(head -n 1 ../README.md | cut -d' ' -f3)
@@ -11,7 +15,7 @@ VERSION=$(head -n 1 ../README.md | cut -d' ' -f3)
 platypus -i 'vinyl.icns' \
          -a 'SSL Scrobbler' \
          -o 'Text Window' \
-         -p '/usr/bin/php' \
+         -p "$(which php)" \
          -V $VERSION \
          -I am.xo.SSLScrobbler \
          -f '../SSL' -f '../config.php-default' -f '../External' -f '../historyreader.php' -f '../README.md' -f '../LICENSE' \
