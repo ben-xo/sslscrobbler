321f60c980baf10fbba6b3cf20dc5e8e6769ea96
Ben XO <75862+ben-xo@users.noreply.github.com>
2025-09-25 14:28:24 +0100

    Update build tool error messages to help me out in a few years time…

--- macOS/build.sh
+++ macOS/build.sh
@@ -7,7 +7,7 @@ if [[ `which platypus` == "" ]]; then
 fi
 
 if [[ `which php` == "" ]]; then
-    echo "Please install php through Mac Homebrew"
+    echo "Please install php through Mac Homebrew, then install the command line tools through the settings"
 fi
 
 VERSION=$(head -n 1 ../README.md | cut -d' ' -f3)
